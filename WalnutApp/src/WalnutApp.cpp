05e6133b1a69434578a4059bbc5c9932fc1b7368
QfiVadis <oskapok@gmail.com>
2023-10-29 18:18:23 +0100

    Display loaded song length

--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -16,9 +16,14 @@
 #include <mpg123.h>
 #include <out123.h>
 
+
+constexpr uint_fast16_t SampleRate = 48000;
+
 class BotLayer : public Walnut::Layer
 {
 public:
+	void LoadAndPlaySong(const std::string& songPath);
+
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Songs");
@@ -26,19 +31,22 @@ public:
 		{
 			if(ImGui::Button(songPath.c_str()))
 			{
-				std::vector<uint8_t> SongData;
-				LoadSong(songPath, SongData);
-				PlaySong(SongData);
+				LoadAndPlaySong(songPath);
 			}
 		}
 		ImGui::End();
 
 
 		ImGui::Begin("Media Controls");
+		ImGui::Text(CurrentSongLengthString.c_str());
 		
 		if(ImGui::Button("StopSong"))
 		{
 			StopSong();
+		}
+		if(ImGui::Button("Pause/Unpause"))
+		{
+			PauseOrUnpauseSong();
 		}
 		ImGui::End();
 	}
@@ -49,13 +57,75 @@ public:
 	void LoadSong(std::string songPath, std::vector<uint8_t>& SongData);
 	void PlaySong(std::vector<uint8_t>& songData);
 	void StopSong();
+	void PauseOrUnpauseSong();
+
+	std::string GetSongDuration(const double& fullSeconds);
 
 private:
 	std::vector<std::string> songPaths;
 	dpp::cluster* bot;
 	dpp::snowflake guildId;
 	dpp::discord_client* discordClient = nullptr;
+
+	long SongLengthInSamples = 0;
+	double SongLengthInSeconds = 0;
+
+	std::string CurrentSongLengthString;
+	
 };
+
+void BotLayer::LoadAndPlaySong(const std::string& songPath)
+{
+	std::vector<uint8_t> SongData;
+	SongData.reserve(50000000);
+	
+
+	mpg123_init();
+	int err = 0;
+	unsigned char* buffer;
+	size_t buffer_size, done;
+	int channels, encoding;
+	long rate;
+ 
+	/* Note it is important to force the frequency to 48000 for Discord compatibility */
+	mpg123_handle *mh = mpg123_new(NULL, &err);
+	mpg123_param(mh, MPG123_FORCE_RATE, SampleRate, SampleRate);
+ 
+	/* Decode entire file into a vector. You could do this on the fly, but if you do that
+	* you may get timing issues if your CPU is busy at the time and you are streaming to
+	* a lot of channels/guilds.
+	*/
+	
+	buffer_size = mpg123_outblock(mh);
+	buffer = new unsigned char[buffer_size];
+ 
+	/* Note: In a real world bot, this should have some error logging */
+	mpg123_open(mh, songPath.c_str());
+	mpg123_scan(mh);
+	mpg123_getformat(mh, &rate, &channels, &encoding);
+
+	SongLengthInSamples = mpg123_length(mh);
+	SongLengthInSeconds = static_cast<double>(SongLengthInSamples) / SampleRate;
+
+	CurrentSongLengthString = GetSongDuration(SongLengthInSeconds);
+
+
+	unsigned int counter = 0;
+	for (int totalBytes = 0; mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK; ) {
+		for (size_t i = 0; i < buffer_size; i++) {
+			SongData.emplace_back(buffer[i]);
+		}
+		counter += buffer_size;
+		totalBytes += done;
+	}
+	delete[] buffer;
+	mpg123_close(mh);
+	mpg123_delete(mh);
+	/* Clean up */
+	mpg123_exit();
+
+	PlaySong(SongData);
+}
 
 void BotLayer::OnAttach()
 {
@@ -133,45 +203,6 @@ void BotLayer::OnDetach()
 	Layer::OnDetach();
 }
 
-void BotLayer::LoadSong(std::string songPath, std::vector<uint8_t>& SongData)
-{
-	mpg123_init();
-	int err = 0;
-	unsigned char* buffer;
-	size_t buffer_size, done;
-	int channels, encoding;
-	long rate;
- 
-	/* Note it is important to force the frequency to 48000 for Discord compatibility */
-	mpg123_handle *mh = mpg123_new(NULL, &err);
-	mpg123_param(mh, MPG123_FORCE_RATE, 48000, 48000.0);
- 
-	/* Decode entire file into a vector. You could do this on the fly, but if you do that
-	* you may get timing issues if your CPU is busy at the time and you are streaming to
-	* a lot of channels/guilds.
-	*/
-	buffer_size = mpg123_outblock(mh);
-	buffer = new unsigned char[buffer_size];
- 
-	/* Note: In a real world bot, this should have some error logging */
-	mpg123_open(mh, songPath.c_str());
-	mpg123_getformat(mh, &rate, &channels, &encoding);
- 
-	unsigned int counter = 0;
-	for (int totalBytes = 0; mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK; ) {
-		for (size_t i = 0; i < buffer_size; i++) {
-			SongData.push_back(buffer[i]);
-		}
-		counter += buffer_size;
-		totalBytes += done;
-	}
-	delete[] buffer;
-	mpg123_close(mh);
-	mpg123_delete(mh);
-	/* Clean up */
-	mpg123_exit();
-}
-
 void BotLayer::PlaySong(std::vector<uint8_t>& songData)
 {
 	dpp::voiceconn* v = discordClient->get_voice(guildId);
@@ -185,6 +216,21 @@ void BotLayer::StopSong()
 {
 	dpp::voiceconn* v = discordClient->get_voice(guildId);
 	v->voiceclient->stop_audio();
+}
+
+void BotLayer::PauseOrUnpauseSong()
+{
+	dpp::voiceconn* v = discordClient->get_voice(guildId);
+	v->voiceclient->pause_audio(!v->voiceclient->is_paused() ? true : false);
+}
+
+std::string BotLayer::GetSongDuration(const double& fullSeconds)
+{
+	int hours = fullSeconds/3600;
+	int minutes = fullSeconds/60;
+	int seconds = fullSeconds - (hours * 3600) - (minutes*60);
+	
+	return std::to_string(hours) + ":"+ std::to_string(minutes) +":"+ std::to_string(seconds);
 }
 
 
